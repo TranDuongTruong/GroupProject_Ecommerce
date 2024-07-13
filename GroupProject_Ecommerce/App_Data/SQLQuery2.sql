a37f098053c94404421cbc94275e95bb2729a6a4
TUDG <tranduongtruong1623@gmail.com>
2024-07-13 07:31:42 +0700

    Add transaction table

---
+++ GroupProject_Ecommerce/App_Data/SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Transactions (
+    TransactionID INT PRIMARY KEY IDENTITY(1,1),
+    ProductID INT,
+    ShopID INT,
+    Quantity INT,
+    Price DECIMAL(18, 2),
+    TransactionDate DATETIME,
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+        FOREIGN KEY (ShopID) REFERENCES Shops(ShopID)
+
+);
